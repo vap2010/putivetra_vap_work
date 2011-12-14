faec85cffb485cbcdd04019a61e684790ddadf28
Nick Recobra <oruenu@gmail.com>
2011-12-15 02:03:35 +0400

    Скрыты лишние поля из редактирования бренда.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,4 +8,13 @@ RailsAdmin.config do |config|
       end
     end
   end
+
+  if Brand.table_exists?
+    config.model Brand do
+      edit do
+        include_all_fields
+        exclude_fields :brand_product_type, :brand_regions, :brand_agencies, :brand_distributors
+      end
+    end
+  end
 end
