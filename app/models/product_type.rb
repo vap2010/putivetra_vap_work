b7e47ba164d98da83906063af0442e97b9b251f6
Nick Recobra <oruenu@gmail.com>
2011-12-15 02:09:43 +0400

    Убраны ненужные ассоциации из интерфейса редактирования.

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -2,6 +2,4 @@ class ProductType < ActiveRecord::Base
   validates :title, :presence => true
   has_many :brand_product_type, :dependent => :destroy
   has_many :brands, :through => :brand_product_type
-  has_many :brand_regions, :dependent => :destroy
-  has_many :regions, :through => :brand_regions
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -17,4 +17,68 @@ RailsAdmin.config do |config|
       end
     end
   end
+
+  if Agency.table_exists?
+    config.model Agency do
+      edit do
+        include_all_fields
+        exclude_fields :brand_agencies
+      end
+    end
+  end
+
+  if Distributor.table_exists?
+    config.model Distributor do
+      edit do
+        include_all_fields
+        exclude_fields :brand_distributors
+      end
+    end
+  end
+
+  if Event.table_exists?
+    config.model Event do
+      edit do
+        include_all_fields
+        exclude_fields :project_events
+      end
+    end
+  end
+
+  if ProductType.table_exists?
+    config.model ProductType do
+      edit do
+        include_all_fields
+        exclude_fields :brand_product_type
+      end
+    end
+  end
+
+  if Project.table_exists?
+    config.model Project do
+      edit do
+        include_all_fields
+        exclude_fields :project_vacancies, :project_events
+      end
+    end
+  end
+
+  if Region.table_exists?
+    config.model Region do
+      edit do
+        include_all_fields
+        exclude_fields :brand_regions
+      end
+    end
+  end
+
+  if Vacancy.table_exists?
+    config.model Vacancy do
+      edit do
+        include_all_fields
+        exclude_fields :project_vacancies
+      end
+    end
+  end
+
 end
