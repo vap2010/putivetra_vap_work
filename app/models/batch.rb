ebff206610b45b2c614ca8b2322c8dd6f5a28882
Nick Recobra <oruenu@gmail.com>
2011-12-27 00:41:36 +0400

    Опечатка.

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -5,7 +5,7 @@ class Batch < ActiveRecord::Base
   has_one :equipment_album, :class_name => "Album", :dependent => :destroy, :as => :albumable, :conditions => {:role => 'equipment'}
   has_one :optional_equipment_album, :class_name => "Album", :dependent => :destroy, :as => :albumable, :conditions => {:role => 'optional'}
   has_one :description_album, :class_name => "Album", :dependent => :destroy, :as => :albumable, :conditions => {:role => 'description'}
-  has_many :samles, :dependent => :destroy
+  has_many :samples, :dependent => :destroy
 
   before_create :build_equipment_album, :build_optional_equipment_album, :build_description_album
 end
