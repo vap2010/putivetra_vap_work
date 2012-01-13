d3933c9ca54aede1fb8d8cfa1ccd1d16f60d55f3
Nick Recobra <oruenu@gmail.com>
2012-01-13 04:01:41 +0400

    Корни для статей.

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,10 @@ class Article < ActiveRecord::Base
   has_many :children, :class_name => 'Article', :order => 'position', :foreign_key => :parent_id, :dependent => :destroy
   belongs_to :project
   validates :project_id, :title, :presence => true
+
+  class << self
+    def roots
+      where(:parent_id => nil)
+    end
+  end
 end
