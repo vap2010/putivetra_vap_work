13b13358855bf106a4a4642fe728f62f40fff3a6
Nick Recobra <oruenu@gmail.com>
2012-02-06 02:16:24 +0400

    Выбор бренда для редактирования.

--- app/assets/javascripts/project_selector.js.coffee
+++ app/assets/javascripts/project_selector.js.coffee
@@ -12,3 +12,16 @@ $ ->
     selectProj($(this).attr("rel"))
     document.location.reload()
   )
+
+  $brands = $(".brands-well .btn")
+  selectBrand = (id) ->
+    $brands.removeClass("primary")
+    $brands.filter($("*[rel=\"#{id}\"]")).addClass("primary")
+    $.cookie('brand', id)
+  val = $.cookie('brand') || 0
+  selectBrand(val)
+  $brands.click(->
+    selectBrand($(this).attr("rel"))
+    document.location.reload()
+  )
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
     end
     content_tag :div, links.join.html_safe, :class => "well projects-well"
   end
+
+  def project_selected?
+    cookies['proj'].to_i > 0
+  end
+
+  def top_brands
+    links = []
+    links << content_tag(:h4, "Выбор бренда", :class => "brands-well-header")
+    links << link_to("Без бренда", "javascript://", :class => "btn", :rel => 0)
+    links += Brand.all.sort_by {|p| p.title.mb_chars }.map do |p|
+      link_to p.title, "javascript://", :class => "btn", :rel => p.id
+    end
+    content_tag :div, links.join.html_safe, :class => "well brands-well" if project_selected?
+  end
 end
