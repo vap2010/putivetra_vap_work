174f94bdcee512931f812743fa2f68e1d5a3d86f
Nick Recobra <oruenu@gmail.com>
2012-02-05 17:40:10 +0400

    Список ссылок на проекты.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def top_projects
+    links = []
+    links << link_to("Без проекта", "javascript://", :class => "btn #{"primary" if cookies[:proj].blank?}")
+    links += Project.all.sort_by {|p| p.title.mb_chars }.map do |p|
+      link_to p.title, {:project_id => p.id}, :class => "btn #{"primary" if cookies[:proj].to_i == p.id}"
+    end
+    links.join.html_safe
+  end
 end
