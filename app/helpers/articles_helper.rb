a556bf3d97245f714ae0f26442cf13edc6d2e3b3
Nick Recobra <oruenu@gmail.com>
2012-03-01 20:33:22 +0100

    Обязательный уникальный урл у метатега и вывод родителя страницы.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,17 @@
 module ArticlesHelper
   def article_submenu article
-    article.children.map do |child|
-      link_to child.title, article_url(child)
-    end.join.html_safe
+    content_tag :ul do
+      article.children.map do |child|
+        content_tag :li, link_to(child.title, article_url(child))
+      end.join.html_safe
+    end
+  end
+
+  def article_parent article
+    content_tag(:p) do
+      "Parent: ".html_safe +
+      link_to(article.parent.title, article_url(article.parent))
+    end if article.parent_id?
   end
 
   def article_url article
